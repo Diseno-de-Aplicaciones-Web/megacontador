02e18d9258647c81ec34dff6098d65fc5d07e31a
DanBaDo <bytes4life@proton.me>
2023-02-22 18:54:51 +0000

    Contador de un solo compoñente con dous botóns

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ function App() {
 
   let [valorActual, setValorActual] = useState(0)
 
-  function manexadorClick() {
+  function manexadorClickIncrementar() {
     let novoValor = valorActual + 1
     setValorActual(novoValor)
   }
 
 
+  function manexadorClickDiminuir() {
+    let novoValor = valorActual - 1
+    setValorActual(novoValor)
+  }
+
   return (
     <>
       <h1>{valorActual}</h1>
-      <button onClick={manexadorClick}>Contador</button>
+      <button onClick={manexadorClickIncrementar}>+</button>
+      <button onClick={manexadorClickDiminuir}>-</button>
     </>
 
   );
