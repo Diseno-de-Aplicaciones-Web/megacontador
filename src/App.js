43cae69b35e2d4d25d38e1c62920f3fa8a9f56e1
DanBaDo <bytes4life@proton.me>
2023-02-23 04:07:06 +0000

    Cada botón registra el número de veces que es pulsado

--- src/App.js
+++ src/App.js
@@ -3,26 +3,39 @@ import { useState } from "react";
 function App() {
 
   let [valorActual, setValorActual] = useState(0)
+  let [numeroIncrementos, setNumeroDeUsos] = useState(0)
+  let [numeroDecrementos, setNumeroDeDecrementos] = useState(0)
 
-  function manexadorClickIncrementar() {
+  function incrementar() {
     let novoValor = valorActual + 1
     setValorActual(novoValor)
   }
 
-
-  function manexadorClickDiminuir() {
+  function diminuir() {
     let novoValor = valorActual - 1
     setValorActual(novoValor)
+  }
+
+  function manexadorClickIncrementar() {
+    const novoNumeroDeUsos = numeroIncrementos+1
+    setNumeroDeUsos(novoNumeroDeUsos)
+    incrementar()
+  }
+
+  function manexadorClickDiminuir() {
+    const novoNumeroDeUsos = numeroDecrementos+1
+    setNumeroDeDecrementos(novoNumeroDeUsos)
+    diminuir()
   }
 
   return (
     <>
       <h1>{valorActual}</h1>
-      <button onClick={manexadorClickIncrementar}>+</button>
-      <button onClick={manexadorClickDiminuir}>-</button>
+      <button onClick={manexadorClickIncrementar}>+({numeroIncrementos})</button>
+      <button onClick={manexadorClickDiminuir}>-({numeroDecrementos})</button>
     </>
-
-  );
+  )
+  
 }
 
 export default App;
